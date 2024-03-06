825ce56b1a6d0ffcc2d26de8f19ac32b1d05124d
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-03-06 19:34:16 +0700

    [REFACTOR] Extract method for validating VoucherCode

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentVoucher.java
@@ -13,28 +13,29 @@ public class PaymentVoucher extends Payment {
     public PaymentVoucher(String id, String method, Map<String, String> paymentData, String status) {
         super(id, method, paymentData, status);
     }
-
+    
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
         final String VOUCHER_CODE_KEY = "voucherCode";
-
         if (paymentData.isEmpty()) {
             throw new IllegalArgumentException();
         } else {
-            int numericalChar = 0;
-            for (char c : paymentData.get(VOUCHER_CODE_KEY).toCharArray()) {
-                if (Character.isDigit(c)) {
-                    numericalChar++;
-                }
-            }
-
             this.paymentData = paymentData;
-            
-            if (paymentData.get(VOUCHER_CODE_KEY).length() == 16 && paymentData.get(VOUCHER_CODE_KEY).startsWith("ESHOP") && numericalChar == 8) {
-                this.status = PaymentStatus.SUCCESS.getValue();
-            } else {
-                this.status = PaymentStatus.REJECTED.getValue();
+            this.status = isValidVoucherCode(paymentData.get(VOUCHER_CODE_KEY))
+                    ? PaymentStatus.SUCCESS.getValue()
+                    : PaymentStatus.REJECTED.getValue();
+        }
+    }
+
+    private boolean isValidVoucherCode(String voucherCode) {
+        int numericalChar = 0;
+        for (char c : voucherCode.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numericalChar++;
             }
         }
+        return voucherCode.length() == 16
+                && voucherCode.startsWith("ESHOP")
+                && numericalChar == 8;
     }
 }
