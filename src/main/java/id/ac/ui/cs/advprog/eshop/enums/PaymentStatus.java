7957be24630345bba8119d85afafb3b41c4d28f4
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-03-06 20:29:03 +0700

    [REFACTOR] Revert change in PaymentStatus enums

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -2,8 +2,6 @@ package id.ac.ui.cs.advprog.eshop.enums;
 
 import lombok.Getter;
 
-import java.util.Arrays;
-
 @Getter
 public enum PaymentStatus {
     SUCCESS("SUCCESS"),
@@ -12,12 +10,16 @@ public enum PaymentStatus {
 
     private final String value;
 
-    PaymentStatus(String value) {
+    private PaymentStatus(String value) {
         this.value = value;
     }
 
-    public static boolean contains(String param) {
-        return Arrays.stream(PaymentStatus.values())
-                .anyMatch(paymentStatus -> paymentStatus.getValue().equals(param));
+    public static boolean contains (String param) {
+        for (PaymentStatus paymentStatus : PaymentStatus.values()) {
+            if (paymentStatus.getValue().equals(param)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
