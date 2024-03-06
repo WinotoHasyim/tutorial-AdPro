57551bebe7a1b9535d2e54e0a68b1e2c9678016a
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-03-06 20:31:47 +0700

    [REFACTOR] Styling

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -8,10 +8,7 @@ import java.util.Map;
 
 public interface PaymentService {
     public Payment addPayment(Order order, String method, Map<String, String> paymentData);
-
     public Payment setStatus(Payment payment, String status);
-
     public Payment getPayment(String paymentId);
-
     public List<Payment> getAllPayments();
 }
