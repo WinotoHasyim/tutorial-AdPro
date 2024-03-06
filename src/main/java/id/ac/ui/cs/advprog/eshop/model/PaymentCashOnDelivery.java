91e26bb76d13262fedeabed8ecc2ebbe0dc76e0f
Winoto Hasyim <winoto.hasyim@ui.ac.id>
2024-03-06 20:20:41 +0700

    [REFACTOR] extract method and variables

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDelivery.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDelivery.java
@@ -5,6 +5,8 @@ import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import java.util.Map;
 
 public class PaymentCashOnDelivery extends Payment {
+    private static final String ADDRESS_KEY = "address";
+    private static final String DELIVERY_FEE_KEY = "deliveryFee";
 
     public PaymentCashOnDelivery(String id, String method, Map<String, String> paymentData) {
         super(id, method, paymentData);
@@ -12,13 +14,15 @@ public class PaymentCashOnDelivery extends Payment {
 
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
-        if (paymentData.isEmpty() || 
-            paymentData.get("address").isEmpty() || 
-            paymentData.get("deliveryFee").isEmpty()) {
+        if (paymentData.isEmpty() || isNullOrEmpty(paymentData.get(ADDRESS_KEY)) || isNullOrEmpty(paymentData.get(DELIVERY_FEE_KEY))) {
             this.status = PaymentStatus.REJECTED.getValue();
         } else {
             this.paymentData = paymentData;
             this.status = PaymentStatus.SUCCESS.getValue();
         }
+    }
+
+    private boolean isNullOrEmpty(String str) {
+        return str == null || str.isEmpty();
     }
 }
